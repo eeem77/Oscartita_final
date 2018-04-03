fe9adf45b2f1af2ed1c470ae63959c30785c5741
eeem77 <escobarmernestoe@gmail.com>
2018-04-03 15:06:09 -0400

    Proyecto_oscartita
    se crea nuevo repositorio dl proyecto oscartita

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Course\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'Course\Model' => 'Course\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Oscartita C.A.</title>
+		<link rel="stylesheet" type="text/css" href="{{asset('assets/css/main.css')}}">
+        <noscript><link rel="stylesheet" type="text/css" href="{{asset('assets/css/noscript.css')}}"></noscript>
+    </head>
+    <body>
+
+        @yield('content')
+
+        <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+        <script src="{{asset('assets/js/skel.min.js')}}"></script>
+        <script src="{{asset('assets/js/util.js')}}"></script>
+        <script src="{{asset('assets/js/main.js')}}"></script>
+    </body>
+</html>
